0cfa17d421230328a7a23310a66e77fbb13bd89c
ecourse <smkang@codenuri.co.kr>
2023-03-07 14:46:45 +0900

    Add files via upload

--- DAY2/ICalc.h
+++ DAY2/ICalc.h
@@ -1,9 +1,20 @@
 // ICalc.h
 #pragma once
 
-struct ICalc
+// 모든 종류의 Proxy 는 결국 참조 계수 함수가 필요 하다.
+struct IRef
+{
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+
+	virtual ~IRef() {}
+};
+
+struct ICalc : public IRef
 {
 	virtual int Add(int, int) = 0;
 	virtual int Sub(int, int) = 0;
 	virtual ~ICalc() {}
 };
+
+// cl calcproxy.cpp /LD
